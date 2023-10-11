166ddd797d8cbeca6dd847eb7252795f3ff7ff0b
antivirus410 <david200388@gmail.com>
2023-10-11 09:37:20 +0200

    javitas torpidostore

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.*;
+//import static org.mockito.Mockito.*;
 
 public class GT4500Test {
 
@@ -22,7 +22,7 @@ public class GT4500Test {
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
-    // Assert
+    // Asserts
     assertEquals(true, result);
   }
 
